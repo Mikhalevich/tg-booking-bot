129246cdee0d70315a2eb3852dbfca718000e884
Michael Mikhalevich <michael.mikhalevich@gmail.com>
2024-11-15 21:26:55 +0300

    scheduler init

---
+++ cmd/schedule/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/Mikhalevich/tg-booking-bot/internal/app/tgbot"
+	"github.com/Mikhalevich/tg-booking-bot/internal/config"
+	"github.com/Mikhalevich/tg-booking-bot/internal/infra"
+	"github.com/Mikhalevich/tg-booking-bot/internal/infra/logger"
+	"github.com/Mikhalevich/tg-booking-bot/internal/infra/tracing"
+)
+
+func main() {
+	var cfg config.ScheduleBot
+	if err := infra.LoadConfig(&cfg); err != nil {
+		logger.StdLogger().WithError(err).Error("failed to load config")
+		os.Exit(1)
+	}
+
+	log, err := infra.SetupLogger(cfg.LogLevel)
+	if err != nil {
+		logger.StdLogger().WithError(err).Error("failed to setup logger")
+		os.Exit(1)
+	}
+
+	if err := runService(cfg, log); err != nil {
+		log.WithError(err).Error("failed run service")
+		os.Exit(1)
+	}
+}
+
+func runService(cfg config.ScheduleBot, log logger.Logger) error {
+	if err := tracing.SetupTracer(cfg.Tracing.Endpoint, cfg.Tracing.ServiceName, ""); err != nil {
+		return fmt.Errorf("setup tracer: %w", err)
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	log.Info("starting bot")
+
+	if err := tgbot.Start(ctx, cfg.Bot.Token); err != nil {
+		return fmt.Errorf("start bot: %w", err)
+	}
+
+	log.Info("bot stopped")
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,33 @@
+module github.com/Mikhalevich/tg-booking-bot
+
+go 1.23.1
+
+require (
+	github.com/go-telegram/bot v1.10.1
+	github.com/jinzhu/configor v1.2.2
+	github.com/sirupsen/logrus v1.9.3
+	go.opentelemetry.io/otel v1.32.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.32.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.32.0
+	go.opentelemetry.io/otel/sdk v1.32.0
+	go.opentelemetry.io/otel/trace v1.32.0
+)
+
+require (
+	github.com/BurntSushi/toml v1.2.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.23.0 // indirect
+	go.opentelemetry.io/otel/metric v1.32.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.3.1 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20241104194629-dd2ea8efbc28 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241104194629-dd2ea8efbc28 // indirect
+	google.golang.org/grpc v1.67.1 // indirect
+	google.golang.org/protobuf v1.35.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
