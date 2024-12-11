1dd0fbd220eef76912dc08090e9a8e2faefb2f92
Michael Mikhalevich <michael.mikhalevich@gmail.com>
2024-12-11 20:19:59 +0300

    create employee refactor

--- internal/domain/employee/create_employee.go
+++ internal/domain/employee/create_employee.go
@@ -2,47 +2,74 @@ package employee
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 
 	"github.com/google/uuid"
 
 	"github.com/Mikhalevich/tg-booking-bot/internal/domain/employee/internal/actionpayload"
+	"github.com/Mikhalevich/tg-booking-bot/internal/domain/employee/internal/button"
+	"github.com/Mikhalevich/tg-booking-bot/internal/domain/internal/ctxdata"
 	"github.com/Mikhalevich/tg-booking-bot/internal/domain/port"
 	"github.com/Mikhalevich/tg-booking-bot/internal/domain/port/action"
 	"github.com/Mikhalevich/tg-booking-bot/internal/domain/port/role"
 )
 
 func (e *employee) CreateEmployee(ctx context.Context, info port.MessageInfo) error {
-	verificationCode := uuid.NewString()
+	currentEmployee, ok := ctxdata.Employee(ctx)
+	if !ok {
+		return errors.New("unable to get current employee")
+	}
 
-	createdEmpID, err := e.repository.CreateEmployee(ctx, role.Employee, verificationCode)
-	if err != nil {
-		return fmt.Errorf("create employee: %w", err)
+	var (
+		verificationCode      string
+		editFirstNameActionID int
+	)
+
+	if err := e.repository.Transaction(
+		ctx,
+		func(ctx context.Context, tx port.EmployeeRepository) error {
+			verificationCode = uuid.NewString()
+
+			createdEmplID, err := tx.CreateEmployee(ctx, role.Employee, verificationCode)
+			if err != nil {
+				return fmt.Errorf("create employee: %w", err)
+			}
+
+			actionPayload, err := actionpayload.BytesFromEmployeeID(createdEmplID)
+			if err != nil {
+				return fmt.Errorf("convert employee id to action payload: %w", err)
+			}
+
+			editFirstNameActionID, err = tx.AddAction(ctx, &action.ActionInfo{
+				EmployeeID: currentEmployee.ID,
+				Action:     action.EditEmployeeFirstName,
+				Payload:    actionPayload,
+				CreatedAt:  time.Now(),
+			})
+
+			if err != nil {
+				return fmt.Errorf("edit employee first name action: %w", err)
+			}
+
+			return nil
+		},
+	); err != nil {
+		return fmt.Errorf("create employee transaction: %w", err)
 	}
 
 	e.sender.ReplyTextMarkdown(ctx, info.ChatID, info.MessageID,
-		fmt.Sprintf("sent this code to user to complete registration: _%s_", e.sender.EscapeMarkdown(verificationCode)),
+		fmt.Sprintf("Sent this code to user to complete registration: _%s_", e.sender.EscapeMarkdown(verificationCode)),
 	)
 
-	currentEmployee, err := e.repository.GetEmployeeByChatID(ctx, info.ChatID)
-	if err != nil {
-		return fmt.Errorf("get employee by chat_id: %w", err)
-	}
-
-	actionPayload, err := actionpayload.BytesFromEmployeeID(createdEmpID)
-	if err != nil {
-		return fmt.Errorf("convert employee id to action payload: %w", err)
-	}
-
-	if _, err := e.repository.AddAction(ctx, &action.ActionInfo{
-		EmployeeID: currentEmployee.ID,
-		Action:     action.EditEmployeeFirstName,
-		Payload:    actionPayload,
-		CreatedAt:  time.Now(),
-	}); err != nil {
-		return fmt.Errorf("edit employee first name action: %w", err)
-	}
+	e.sender.ReplyText(ctx, info.ChatID, info.MessageID,
+		"Employee created. Please enter employee first name",
+		button.CancelButton("Cancel", button.ActionData{
+			ID:   editFirstNameActionID,
+			Type: button.ActionTypeCancel,
+		}),
+	)
 
 	return nil
 }
